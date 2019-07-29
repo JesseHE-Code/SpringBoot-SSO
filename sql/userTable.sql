5293355c37bc4bcc3016af936588391ce1630cd5
hezhiqiang <1187649824@qq.com>
2019-07-29 11:25:02 +0800

    sql更新
    sql文件更新

--- sql/userTable.sql
+++ sql/userTable.sql
@@ -7,4 +7,6 @@ CREATE TABLE `sso_service`.`userTable` (
   `user_role` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`user_id`));
 
+insert into sso_service.userTable(`user_name`,`user_password`,`user_role`) values("hezhiqiang", "123456","NORMAL");
+
 SELECT * FROM sso_service.userTable;
